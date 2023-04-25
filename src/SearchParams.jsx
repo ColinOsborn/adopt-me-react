97695f9c63eb676cad37c1a0b7228d581c06ded6
Colin Osborn <colin.osborn@gmail.com>
2023-04-25 07:51:48 -0600

    implemented more logic in searchParams to allow for more flexible location and animals array to select from

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
-  const location = "Aspen, CO";
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
